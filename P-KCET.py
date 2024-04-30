2cd7fded5992d34e2bf294a6d6a6d62f5ae33d68
Shreejit R Patil <155508503+Shreejit22@users.noreply.github.com>
2024-05-01 00:14:33 +0530

    Program for Checking K-CET Rankings for all Branches of GIT

---
+++ P-KCET.py
@@ -0,0 +1,33 @@
+print("       -------------------K-CET Ranking for GOGTE INSTITUTE OF TECHNOLOGY--------------         ")
+
+print("Hello"+" "+input("What's your Name : ")+" "+"Showing all the branches of Engineering below")
+print('\t')
+print("Aeronautical Engineering")
+print("Computer Science")
+print("Electricals and Electronics")
+print("Electronics and Communication")
+print("Chemical")
+print("Information Science")
+print("Mechanical")
+print("Civil")
+print('\t')
+Branch=input("Which Branch do u want in GIT : ")
+
+if Branch=="CS":
+      print("Ranking must be below 8000")
+elif Branch=="EC":
+      print("Ranking must be below 17000")
+elif Branch=="IS":
+      print("Ranking must be below 13000")
+elif Branch=="EE":
+      print("Ranking must be below 30000")
+elif Branch=="AE":
+      print("Ranking must be below 39000")
+elif Branch=="Civil":
+      print("Ranking must be below 100000")
+elif Branch=="EC":
+      print("Ranking must be below 86000")
+else:
+      printf("ALL THE BEST FOR YOUR FUTURE")
+
+
